76b10fe817c4781e4f90ca0c682461009980bdac
Shawn Clake <shawn.clake@gmail.com>
2018-08-28 10:37:44 -0600

    Update SymbolScope.py

--- SymbolScope.py
+++ SymbolScope.py
@@ -4,3 +4,26 @@ from something.Symbol import Symbol
 class SymbolScope:
     def __init__(self):
         self.symbols = []
+
+    def addSymbol(self, symbol: Symbol):
+        if(self.getSymbol(symbol.name, symbol.symbolType)):
+            return False
+        self.symbols.append(symbol)
+        return True
+    
+    def removeSymbol(self, symbol: Symbol):
+        symbol = self.getSymbol(symbol.name, symbol.symbolType)
+        if(not symbol):
+            return False
+        self.symbols.remove(symbol)
+        
+    def getSymbol(self, name, symbolType=None):
+        for symbol in self.symbols:
+            if(symbol.name == name):
+                if(not symbolType):
+                    return symbol
+                else:
+                    if(symbol.symbolType == symbolType):
+                        return symbol
+        return None  
+                    
