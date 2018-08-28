e71494ac715c5603d3e0f5a012e32e46180eb79f
Shawn Clake <shawn.clake@gmail.com>
2018-08-28 10:22:01 -0600

    Create Symbol.py

---
+++ Symbol.py
@@ -0,0 +1,8 @@
+
+
+
+class Symbol:
+    def __init__(self, symbolType=None, dataType=None, value=None):
+        self.symbolType = symbolType
+        self.dataType = dataType
+        self.value = value
