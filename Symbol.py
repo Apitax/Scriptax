75d133884300297fbb23305ae1b77d4c670d3787
Shawn Clake <shawn.clake@gmail.com>
2018-08-28 10:30:23 -0600

    Update Symbol.py

--- Symbol.py
+++ Symbol.py
@@ -2,7 +2,8 @@
 
 
 class Symbol:
-    def __init__(self, symbolType=None, dataType=None, value=None):
+    def __init__(self, name=None, symbolType=None, dataType=None, value=None):
+        self.name = name
         self.symbolType = symbolType
         self.dataType = dataType
         self.value = value
